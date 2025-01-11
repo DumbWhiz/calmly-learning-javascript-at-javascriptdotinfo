3b35e18952c24e218e909fb523b2823278de1e59
DumbWhiz <adeyeyewilson@gmail.com>
2025-01-11 06:30:03 +0100

    loop-while-do-while

---
+++ script/loop.js
@@ -0,0 +1,38 @@
+//LOOPS : While & For
+
+//While Loop
+
+/* while(condition){
+    //code
+    //so called loop body
+}
+ */
+/* let i = 0;
+while (i < 3){
+    alert( i ); //code
+     i++; //so called loop body
+} */
+//
+/* let i=0;
+while(i<3) alert(i++); */
+
+//"DO...WHILE" LOOP
+
+/* do {
+   //Loop Body
+} while (condition); */
+
+/* let i = 0;
+do {
+    alert(i);
+    i++
+} while (i < 3);
+ */
+//"for" Loop
+
+/* for (begin; condition; step){
+    // ... loop body ...
+}
+ */
+
+for ()
